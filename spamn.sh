5b6a7998009ee260c5af9dd2924bfb680898f4a1
lifespirit <lifespirit@sip3.io>
2023-07-05 13:59:45 +0300

    Add new spam site

--- spamn.sh
+++ spamn.sh
@@ -3,43 +3,46 @@
 botid=bot111222333
 botkey=AAssddffgghhjjkkllqqwwweerrttyyuuiioozxc
 
-number=`echo $1 | sed -e 's/+//g'`
+number=`echo $1 | sed -e 's/+//g' -e 's/8\([0-9]\{10\}\)/7\1/g'`
 to=`echo $2 | sed -e 's/+//g'`
-request=`curl -s https://www.neberitrubku.ru/nomer-telefona/$number | grep "li class='active'" | tr -d "\r\n" | sed -e 's/ \+/ /g' -e 's/^ //g' -e 's/<\//\;<\//g' -e 's/<[^>]*>//g' -e 's/; \+/;/g' -e 's\;;\;\g'`
-OLDIFS=$IFS
-IFS=";"
-fails=0
-goods=0
-for i in $request
-do
-  if [[ `echo $i | grep "трицательн"` ]]
-  then
-    fails=$(($fails+`echo $i | cut -d"x" -f1`))
-  fi
-  if [[ `echo $i | grep "оложительн"` ]]
-  then
-    goods=$(($goods+`echo $i | cut -d"x" -f1`))
-  fi
-done
-IFS=$OLDIFS
-# If you use script with more then 1 number and want many chats uncomment that
-#if [ $to == 71112223344 ]
-#then
-#  chatid="111222333"
-#elif [ $to == 72223334455 ]
-#then
-#  chatid="222333444"
-#elif [ $to == 73334445566 ]
-#then
-#  chatid="333444555"
-#else
-  chatid="444555666"
-#fi
-if [ $(( $fails - $goods )) -gt 0 ]
+whitelist=`grep $number /etc/asterisk/whitelist.txt`
+if [ $to == 70001112233 ]
 then
-  curl -s "https://api.telegram.org/$botid:$botkey/sendMessage?chat_id=$chatid&&parse_mode=html&text=Звонит номер <b>$number</b>.%0A<b>Определено как спам</b>.%0AОценки: $request.%0Ahttps://www.neberitrubku.ru/nomer-telefona/$number" 2>&1 1>/dev/null
-  echo 1
+  chatid="1111111111"
+elif [ $to == 71112223344 ]
+then
+  chatid="2222222222"
 else
-  curl -s "https://api.telegram.org/$botid:$botkey/sendMessage?chat_id=$chatid&&parse_mode=html&text=Звонит номер <b>$number</b>.%0AОценки: $request.%0Ahttps://www.neberitrubku.ru/nomer-telefona/$number" 2>&1 1>/dev/null
-  echo 0
+  chatid="3333333333"
+fi
+if [[ -z "$whitelist" ]]
+then
+  request=`curl -s -L https://callfilter.app/search --data-raw "searchNumber=$number" | grep \<\/li\> | grep class | sed 's/.*>\(.*\)<\/li>/\1/g'`
+  OLDIFS=$IFS
+  IFS=$'\n'
+  fails=0
+  goods=0
+  for i in $request
+  do
+    if [[ `echo $i | grep "трицательн"` ]]
+    then
+      fails=$(($fails+`echo $i | cut -d"x" -f1`))
+    fi
+    if [[ `echo $i | grep "оложительн"` ]]
+    then
+      goods=$(($goods+`echo $i | cut -d"x" -f1`))
+    fi
+  done
+  IFS=$OLDIFS
+  if [ $(( $fails - $goods )) -gt 0 ]
+  then
+    curl -s "https://api.telegram.org/$botid:$botkey/sendMessage?chat_id=$chatid&&parse_mode=html&text=Звонит%20номер%20<b>$number</b>%20.%0A<b>Определено%20как%20спам</b>.%0AОценки:%20`echo $request | sed 's/ /%20/g' | tr "\n" " " | sed 's/ /%0A/g'`.%0Ahttps://callfilter.app/$number" 2>&1 1>/dev/null
+    echo 1
+  else
+    curl -s "https://api.telegram.org/$botid:$botkey/sendMessage?chat_id=$chatid&&parse_mode=html&text=Звонит%20номер%20<b>$number</b>%20.%0AОценки:%20`echo $request | sed 's/ /%20/g' | tr "\n" " " | sed 's/ /%0A/g'`.%0Ahttps://callfilter.app/$number" 2>&1 1>/dev/null
+    echo 0
+  fi
+else
+  curl -s "https://api.telegram.org/$botid:$botkey/sendMessage?chat_id=$chatid&&parse_mode=html&text=Звонит%20номер%20<b>$number</b>%20.%0AНомер%20в%20белом%20списке." 2>&1 1>/dev/null
+  echo 0  
 fi
